a85799b981c9eb99533550eee683537982039630
leo981008 <108375009+leo981008@users.noreply.github.com>
2023-09-20 12:53:46 +0800

    read_sql.py

---
+++ core/read_sql.py
@@ -0,0 +1,21 @@
+# id will auto increase
+# use nextc;
+# CREATE TABLE nextctable1(id int PRIMARY KEY AUTO_INCREMENT, rssi int);
+
+import pymysql.cursors
+
+# Connect to the database
+connection = pymysql.connect(host='127.0.0.1',
+                             user='situser',
+                             password='sit',
+                             database='sitdb',
+                             cursorclass=pymysql.cursors.DictCursor)
+
+cursor = connection.cursor()
+# query = "select '*' from nextctable"
+query = "insert pointdb value(0,111,222,333,444,555)"
+cursor.execute(query)
+query1 = "SELECT * FROM pointdb;"
+cursor.execute(query1)
+print(cursor.fetchall())
+connection.commit()
