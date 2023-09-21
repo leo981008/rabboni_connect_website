78b3bb02c86d04b6ffd7590d6bedac2b553b23c4
leo981008 <108375009+leo981008@users.noreply.github.com>
2023-09-21 12:44:31 +0800

    Delete dashboard/public/WebSocket.js

--- dashboard/public/WebSocket.js
+++
@@ -1,8 +0,0 @@
-var ws = new WebSocket('');
-  function shortPolling() {
-    ws.addEventListener('message', function(e) {
-      var data = JSON.parse(e.data);
-      value = [(Number(data))]
-    })
-  }
-  setInterval(shortPolling, 1000);
