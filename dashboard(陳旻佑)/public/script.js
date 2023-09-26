ccc1cb2f6baf5749f41f1053042a4289c9b3f5d7
ELEPOT <109792465+ELEPOT@users.noreply.github.com>
2023-09-26 21:32:33 +0800

    Update script.js to fetch data. Unfinished

--- dashboard(陳旻佑)/public/script.js
+++ dashboard(陳旻佑)/public/script.js
@@ -35,32 +35,37 @@ for (i = 0; i < stored.length - 1; i++) {
   charts.push(draw_line_chart(stored[i], `chart${i+1}`, `數據${i+1}`));
 }
 
-httpRequest.open("GET", "http://10.240.40.209:5000/", true)
-httpRequest.setRequestHeader('Access-Control-Allow-Headers', '*');
-httpRequest.setRequestHeader('Content-type', 'application/ecmascript');
-httpRequest.setRequestHeader('Access-Control-Allow-Origin', '*');
-httpRequest.send()
-console.log(httpRequest.responseText)
+
 
 function shortPolling() {
-    
-    var data = JSON.parse(httpRequest.open("GET", "http://10.240.40.209:5000/", true));
-    value1 = Number(data.p1);
-    chart1.data.datasets[0].data.push(value);
-    value2 = Number(data.p2);
-    chart2.data.datasets[0].data.push(value);
-    value3 = Number(data.p3);
-    chart3.data.datasets[0].data.push(value);
-    value4 = Number(data.p4);
-    chart4.data.datasets[0].data.push(value);
-    value5 = Number(data.p5);
-    chart5.data.datasets[0].data.push(value);
-    value6 = toString(data.time);    
-    chart1.data.labels.push(value);
-    chart2.data.labels.push(value);
-    chart3.data.labels.push(value);
-    chart4.data.labels.push(value);
-    chart5.data.labels.push(value);
+    httpRequest.open("GET", "http://192.168.68.106:5000", true)
+    httpRequest.setRequestHeader('Access-Control-Allow-Headers', '*');
+    httpRequest.setRequestHeader('Content-type', 'application/ecmascript');
+    httpRequest.setRequestHeader('Access-Control-Allow-Origin', '*');
+    httpRequest.send();
+    httpRequest.onreadystatechange = () => {
+      console.log(httpRequest.responseText);
+      var data = JSON.parse(httpRequest.responseText);
+
+      value1 = Number(data.p1);
+      chart1.data.datasets[0].data.push(value1);
+      value2 = Number(data.p2);
+      chart2.data.datasets[0].data.push(value2);
+      value3 = Number(data.p3);
+      chart3.data.datasets[0].data.push(value3);
+      value4 = Number(data.p4);
+      chart4.data.datasets[0].data.push(value4);
+      value5 = Number(data.p5);
+      chart5.data.datasets[0].data.push(value5);
+      value6 = toString(data.time);    
+      chart1.data.labels.push(value);
+      chart2.data.labels.push(value);
+      chart3.data.labels.push(value);
+      chart4.data.labels.push(value);
+      chart5.data.labels.push(value);
+    };
+
+
 
   //   var ws = new WebSocket("ws:10.240.40.209:5000");
   //  ws.addEventListener("message", function(e) {
