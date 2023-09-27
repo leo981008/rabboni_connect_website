126fbf0790688e1ae0d09b7ff30c93d3194162f9
mailisme <112573163+mailisme@users.noreply.github.com>
2023-09-27 13:12:08 +0800

    DFDGJ?SDJsrjkj?

--- dashboard/public/script.js
+++ dashboard/public/script.js
@@ -2,7 +2,6 @@ Chart.defaults.backgroundColor = "rgb(128, 138, 135)";
 var stored = [[0], [0], [0], [0], [0], [0]];
 const httpRequest = new XMLHttpRequest();
 
-
 function draw_line_chart(data, element_id, label="數據", color="rgb(128, 138, 135)") {
       const ctx = document.getElementById(element_id);
       const chart = new Chart(ctx, {
@@ -38,7 +37,7 @@ for (i = 0; i < stored.length - 1; i++) {
 
 
 function shortPolling() {
-    httpRequest.open("GET", "http://192.168.68.106:5000", true)
+    httpRequest.open("GET", "http://10.240.40.209:5000", true)
     httpRequest.setRequestHeader('Access-Control-Allow-Headers', '*');
     httpRequest.setRequestHeader('Content-type', 'application/ecmascript');
     httpRequest.setRequestHeader('Access-Control-Allow-Origin', '*');
@@ -48,21 +47,21 @@ function shortPolling() {
       var data = JSON.parse(httpRequest.responseText);
 
       value1 = Number(data.p1);
-      chart1.data.datasets[0].data.push(value1);
+      charts[0].data.datasets[0].data.push(value1);
       value2 = Number(data.p2);
-      chart2.data.datasets[0].data.push(value2);
+      charts[1].data.datasets[0].data.push(value2);
       value3 = Number(data.p3);
-      chart3.data.datasets[0].data.push(value3);
+      charts[2].data.datasets[0].data.push(value3);
       value4 = Number(data.p4);
-      chart4.data.datasets[0].data.push(value4);
+      charts[3].data.datasets[0].data.push(value4);
       value5 = Number(data.p5);
-      chart5.data.datasets[0].data.push(value5);
-      value6 = toString(data.time);    
-      chart1.data.labels.push(value);
-      chart2.data.labels.push(value);
-      chart3.data.labels.push(value);
-      chart4.data.labels.push(value);
-      chart5.data.labels.push(value);
+      charts[4].data.datasets[0].data.push(value5);
+      value6 = toString(new Date(data.time*1000));    
+      charts[0].data.labels.push(value6);
+      charts[1].data.labels.push(value6);
+      charts[2].data.labels.push(value6);
+      charts[3].data.labels.push(value6);
+      charts[4].data.labels.push(value6);
     };
    }
 ;
